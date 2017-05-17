b52f53a79c7c1f4c4f3d3acf5b8ae3c78df925a7
Brian Jubelirer <brian2386@gmail.com>
2017-05-16 21:35:33 -0400

    add omniauth

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: "email@email.com", username: "username", password: "password", password_confirmation: "password")
+  end
+
+  test "User is valid" do
+    assert @user.valid?, true
+  end
+  
 end
