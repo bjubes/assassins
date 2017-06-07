bd6d95c2a0090af131c167ea326252cc9fee49aa
Brian Jubelirer <brian2386@gmail.com>
2017-06-07 18:25:20 -0400

    boostrap team

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,17 @@ module ApplicationHelper
     #if !current_user.
       flash[:error] = "You are not authorized to do that"
       redirect_to current_user
-    
+
+  end
+
+  def boostrap_color_from_status(status)
+    return "success" if status == "alive"
+    return "warning" if status == "endangered"
+    return "danger" if status == "dead"
+  end
+
+  def back_link(text="Back")
+    s = %(<a href="#" onclick="history.go(-1);return false;">) + "#{html_escape(text)}</a>"
+    s.html_safe
   end
 end
